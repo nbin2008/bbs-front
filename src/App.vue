397e047c316ecf3f1f3537ed55cfbc3660f10994
nbin <1458282@qq.com>
2019-08-20 14:19:29 +0800

    更改bootstrap引入

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,6 @@
 </template>
 
 <style lang="less">
-@import "./assets/bootstrap-3.3.7-dist/css/bootstrap.min.css";
+@import "~bootstrap/dist/css/bootstrap.min.css";
 @import "./assets/less/app";
 </style>
